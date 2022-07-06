0b6688c4f7a731c2d530a99846d54597921b59e0
Umang-Shriwas <umangshriwas17@gmail.com>
2022-07-06 22:30:07 +0530

    new changes

--- lib/Screens/home2.dart
+++ lib/Screens/home2.dart
@@ -3,36 +3,37 @@ import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 
 class Home2 extends StatefulWidget {
-  const Home2({ Key? key }) : super(key: key);
+  const Home2({Key? key}) : super(key: key);
 
   @override
   State<Home2> createState() => _Home2State();
 }
 
 class _Home2State extends State<Home2> {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('Product').snapshots();
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('Product').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: StreamBuilder<QuerySnapshot>(
-          
           stream: _usersStream,
           builder: (context, snapshot) {
-           if (snapshot.hasError) {
+            if (snapshot.hasError) {
               return Text('Something went wrong');
             }
-      
+
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Text("Loading");
             }
-      
+
             return SizedBox(
               height: 200,
               child: ListView(
                 children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
                   return ListTile(
                     leading: Image(image: NetworkImage(data['product_image'])),
                     title: Text(data['product_name']),
@@ -47,4 +48,4 @@ class _Home2State extends State<Home2> {
       ),
     );
   }
-      }
+}
